76e65bb9606a7bcc59b560032c14f6c83046d771
Leon <leon@mage-profis.de>
2018-06-13 10:52:07 +0200

    wip ajax check

---
+++ src/code/controllers/AjaxController.php
@@ -0,0 +1,18 @@
+<?php
+
+class MageProfis_Spam_AjaxController extends Mage_Core_Controller_Front_Action
+{
+    function indexAction()
+    {
+        Mage::getSingleton('core/session')->setMageProfisSpamAjax(true);
+
+        $result = array(
+            'check' => 'success', 
+        );
+        $this->getResponse()->setHeader('Content-type', 'application/json', true);
+        $this->getResponse()->setHeader('X-Robots-Tag', 'noindex, nofollow', true);
+        $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($result));
+        return $this;
+    }
+
+}
